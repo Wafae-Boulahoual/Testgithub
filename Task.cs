9d27e4971e674558a580d349114e5ab68f7d0395
wafae Boulahoual <wafae.essbai@gmail.com>
2026-09-03 18:59:21 +0200

    Mark tasks as completed

---
+++ Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
