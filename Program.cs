994b3ae56b07303218384b1a33fde90fe62ed956
wafae Boulahoual <wafae.essbai@gmail.com>
2026-09-03 15:40:39 +0200

    add task to list

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hej allihopa!");
+﻿List<string> uppgifter = new List<string>();
+Console.ForegroundColor = ConsoleColor.DarkGray;
+Console.WriteLine("Ange en uppgift:");
+Console.ResetColor();
+string beskrivning = Console.ReadLine();
+
+uppgifter.Add(beskrivning);
+
+Console.WriteLine($"Uppgiften '{beskrivning}' har lagts till!");
